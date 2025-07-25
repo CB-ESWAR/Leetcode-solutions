3daead9741dc406e2287277df8b1f5cfac7d6859
CB ESWAR <cbeswar20072006@gmail.com>
2025-07-25 11:10:53 +0530

    Time: 0 ms (100%), Space: 29.9 MB (12.16%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_map <int,int> freq;
+        //ESWAR
+        int n=nums.size();
+        for(int i=0;i<n;i++)freq[nums[i]]++;
+        vector <int> hi;
+        int count=0;
+        for(int i:nums){
+            if(i<0)count++;
+            if(freq[i] && i>=0){
+                freq[i]=0;
+                hi.push_back(i);
+            }
+        }
+        int neg_sum=INT_MAX;
+        if(count==n){
+           sort(nums.begin(),nums.end());
+           return nums[n-1];
+        }
+        int sum=0;
+        for(int i:hi)sum+=i;
+        return sum;
+    }
+};
