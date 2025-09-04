3ca3e5fb518e075abdbad30e4a344d169d83ad7b
CB ESWAR <cbeswar20072006@gmail.com>
2025-09-04 10:48:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (88.8%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
+        //ESWAR
         int ans=0;
         if(abs(z-x)<abs(z-y))ans=1;
         else if(abs(z-x)>abs(z-y))ans=2;
