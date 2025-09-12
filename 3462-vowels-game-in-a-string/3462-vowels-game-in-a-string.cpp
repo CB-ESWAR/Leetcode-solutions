86f20ea8b5d153a36199af61bedae1cd77406567
CB ESWAR <cbeswar20072006@gmail.com>
2025-09-12 10:07:20 +0530

    Time: 4 ms (78.54%), Space: 19.1 MB (53.73%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
+        //Eswar
         for(int i=0;i<s.length();i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')return true;
 
