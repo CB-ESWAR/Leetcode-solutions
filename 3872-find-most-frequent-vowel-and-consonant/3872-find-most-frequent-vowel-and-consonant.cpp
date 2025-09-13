470e8877375618f4125e4f725471294aa250d429
CB ESWAR <cbeswar20072006@gmail.com>
2025-09-13 09:31:50 +0530

    Time: 8 ms (6.78%), Space: 11.5 MB (5.03%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        string s1="",s2="";
+        unordered_map <char,int> freq;
+        for(int i=0;i<s.size();i++){
+          if(s[i]=='a' || s[i]=='e' || s[i]=='o' || s[i]=='i' || s[i]=='u')s1=s[i]+s1;
+        }
+        for(char c:s1)freq[c]++;
+        int max_vowel=0;
+        int max_con=0;
+        for(char c:s1){
+            max_vowel=max(freq[c],max_vowel);
+        }
+        for(int i=0;i<s.size();i++){
+          if(s[i]!='a' && s[i]!='e' && s[i]!='o' && s[i]!='i' && s[i]!='u')s2=s[i]+s2;
+        }
+        for(char c:s2)freq[c]++;
+         for(char c:s2){
+            max_con=max(freq[c],max_con);
+        }
+      int ans=max_vowel+max_con;
+      return ans;
+    }
+};
