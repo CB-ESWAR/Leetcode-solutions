abebf8244d6381656f454ad2c2b67eb8dda65366
CB ESWAR <cbeswar20072006@gmail.com>
2025-09-28 15:20:54 +0530

    Time: 10 ms (30.97%), Space: 29 MB (5.52%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -4,12 +4,8 @@ public:
         //ESWAR
         sort(nums.rbegin(),nums.rend());
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]<nums[j]+nums[k]) return nums[i]+nums[j]+nums[k];
-                }
-            }   
+        for(int i=0;i<n-2;i++){
+                 if(nums[i]<nums[i+1]+nums[i+2]) return nums[i]+nums[i+1]+nums[i+2];
         }
         return 0;
     }
