ff6f300445db32c65c36f9fcea5dfc37f7ff634d
CB ESWAR <cbeswar20072006@gmail.com>
2025-09-29 19:39:56 +0530

    Time: 3 ms (44.41%), Space: 9.4 MB (29.22%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,9 +4,9 @@ public:
             //ESWAR
         unordered_map<char, int> m1, m2;
         for(int i=0;i<s.size();i++){
-            if (m1[s[i]]!=m2[t[i]])return false;
+            if (m1[s[i]]!=m2[t[i]])return 0;
             m1[s[i]]=m2[t[i]]=i+1;
         }
-         return true;
+         return 1;
     }
 };
