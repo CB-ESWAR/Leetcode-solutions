1c76cf62ccd14d650be9af4407b71ad160a6f33d
CB ESWAR <cbeswar20072006@gmail.com>
2025-11-09 12:14:32 +0530

    Time: 1 ms (25.41%), Space: 8 MB (6.11%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int countOperations(int num1, int num2) {
         int count=0;
+        //ESWAR
         while (num1!=0 && num2!=0) {
             if (num1>=num2) num1-=num2;
             else num2-=num1;
