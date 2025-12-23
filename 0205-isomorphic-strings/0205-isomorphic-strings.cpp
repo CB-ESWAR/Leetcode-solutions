f83c09aec3c4c2d18d0a677ade0522366a7cbad9
CB ESWAR <cbeswar20072006@gmail.com>
2025-12-23 17:28:06 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.79%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-            //ESWAR
-        unordered_map<char, int> m1, m2;
-        for(int i=0;i<s.size();i++){
-            if (m1[s[i]]!=m2[t[i]])return 0;
-            m1[s[i]]=m2[t[i]]=i+1;
-        }
-         return 1;
+       unordered_map<char,int> hi,bye;
+       for(int i=0;i<s.length();i++){
+        if(hi[s[i]]!=bye[t[i]])return 0;
+        hi[s[i]]=bye[t[i]]=i+1;
+       }
+       return true;
     }
 };
