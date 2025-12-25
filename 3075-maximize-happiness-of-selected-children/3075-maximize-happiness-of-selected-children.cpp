3a1c21246fb5b6f172367ed479a572ee4514321a
CB ESWAR <cbeswar20072006@gmail.com>
2025-12-25 11:55:50 +0530

    Time: 200 ms (21.99%), Space: 110.3 MB (18.09%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
+        //ESWAR
         sort(happiness.rbegin(),happiness.rend());
         long long sum=0;
         for(int i=1;i<happiness.size();i++){
             happiness[i]=happiness[i]-i;
             if(happiness[i]<0)happiness[i]=0;
         }
-        for(int i=0;i<k;i++){
-            sum+=happiness[i];
-        }
+        for(int i=0;i<k;i++)sum+=happiness[i];
         return sum;
-
     }
 };
