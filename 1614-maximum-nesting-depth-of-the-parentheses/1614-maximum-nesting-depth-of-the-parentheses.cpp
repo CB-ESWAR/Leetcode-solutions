39522a310424e3f22259ab931a8fac0af88dd5da
CB ESWAR <cbeswar20072006@gmail.com>
2025-12-25 15:14:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (58.85%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int maxi=0;
+        int left=0;
+        int right=0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(')left++;
+            else if(s[i]==')')right++;
+            maxi=max(maxi,count);
+            count=left-right;
+        }
+        return maxi;
+    }
+};
