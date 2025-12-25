0ac4aaceac7ffdc8c7479c4dd0c9f4d0cf46f744
CB ESWAR <cbeswar20072006@gmail.com>
2025-12-25 11:55:40 +0530

    Time: 222 ms (9.93%), Space: 110.2 MB (18.09%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.rbegin(),happiness.rend());
+        long long sum=0;
+        for(int i=1;i<happiness.size();i++){
+            happiness[i]=happiness[i]-i;
+            if(happiness[i]<0)happiness[i]=0;
+        }
+        for(int i=0;i<k;i++){
+            sum+=happiness[i];
+        }
+        return sum;
+
+    }
+};
