eb70490abfac7c37b8f16f9623d74f9f97c0970d
CB ESWAR <cbeswar20072006@gmail.com>
2025-12-25 15:18:37 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.97%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -4,13 +4,10 @@ public:
         //ESWAR
         int maxi=0;
         int left=0;
-        int right=0;
-        int count=0;
         for(int i=0;i<s.length();i++){
             if(s[i]=='(')left++;
-            else if(s[i]==')')right++;
-            maxi=max(maxi,count);
-            count=left-right;
+            else if(s[i]==')')left--;
+            maxi=max(maxi,left);
         }
         return maxi;
     }
