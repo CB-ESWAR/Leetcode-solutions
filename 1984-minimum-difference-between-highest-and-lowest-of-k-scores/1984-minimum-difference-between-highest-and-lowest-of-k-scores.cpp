30346f63751928d714c873ace9b779b924638bcd
CB ESWAR <cbeswar20072006@gmail.com>
2026-01-26 22:17:52 +0530

    Time: 0 ms (100%), Space: 17.4 MB (21.88%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -4,12 +4,9 @@ public:
         sort(nums.begin(),nums.end());
         int mini=INT_MAX;
       if(k==1)return 0;
-      else{
         for(int i=0;i+k-1<nums.size();i++){
-            int diff=nums[i+k-1]-nums[i];
-            mini=min(diff,mini);
+            mini=min(nums[i+k-1]-nums[i],mini);
         }
-      }
         return mini;
     }
 };
