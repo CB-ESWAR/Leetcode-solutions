f56de179f7a79dcca642a016cc604f20f1988950
CB ESWAR <cbeswar20072006@gmail.com>
2026-06-06 19:34:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
+        unordered_map<int,int>map;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                   return{i,j};
-                }
-            }
+            int hi=target-nums[i];
+            if(map.find(hi)!=mp.end())return {mp[hi],i};
+            mp[nums[i]]=i;
         }
         return {};
     }
