b33c544edce7d695617a7c6d1984fed1997da3ec
CB ESWAR <cbeswar20072006@gmail.com>
2026-07-15 11:05:20 +0530

    Time: 560 ms (10.31%), Space: 8.4 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=1;i<2*n;i+=2)odd_sum+=i;
         for(int i=2;i<=2*n;i+=2)even_sum+=i;
         int ans=1;
-        for(int i=1;i<min(even_sum,odd_sum);i++){
+        for(int i=1;i<odd_sum;i++){
             if(even_sum%i==0 && odd_sum%i==0)ans=i;
         }
         return ans;
